27945e490ccd00e1f222578e6f2c7bccb22345b6
Aroueterra <aroueterra@gmail.com>
2018-11-02 12:32:41 +0800

    Constructed user interface base model

--- Sage/MainWindow.xaml.cs
+++ Sage/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using MahApps.Metro.Controls;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -18,11 +19,24 @@ namespace Sage
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            TextElement.FontFamilyProperty.OverrideMetadata(
+            typeof(TextElement),
+            new FrameworkPropertyMetadata(
+            new FontFamily("Segoe UI")));
+            TextBlock.FontFamilyProperty.OverrideMetadata(
+            typeof(TextBlock),
+            new FrameworkPropertyMetadata(
+                new FontFamily("Segoe UI")));
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
